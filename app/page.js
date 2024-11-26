2d1619ff8fdedd72559857f14db795d5191ff0ff
rscherwinai <rscherwin.ai@gmail.com>
2024-11-25 21:56:25 -0500

    3 - SSO Added

--- app/page.js
+++ app/page.js
@@ -93,9 +93,9 @@ export default function ApiKeyDashboard() {
             </a>
             <a
               className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="/dashboards"
+              href="/dashboards/playground"
             >
-              Manage API Keys
+              API Playground
             </a>
           </div>
         </main>
